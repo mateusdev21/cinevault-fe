244ac41ea6faebee2e2103847f1f298ac0afc61e
mateusdev21 <mateus.dev21@gmail.com>
2026-02-19 15:43:46 +0700

    feat(auth): add password visibility toggle with icon on login page

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -3,10 +3,12 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useLogin } from '../hooks/useLogin'
 import { getDefaultRouteByRole } from '../utils/roleRedirect'
+import { IoEyeOutline, IoEyeOffOutline } from 'react-icons/io5'
 
 const LoginPage = () => {
     const navigate = useNavigate()
     const { login, loading } = useLogin()
+    const [showPassword, setShowPassword] = useState(false)
 
     const [form, setForm] = useState({
         email: '',
@@ -39,27 +41,59 @@ const LoginPage = () => {
 
             <form onSubmit={handleSubmit} className="space-y-4">
                 <div>
-                    <label className="block text-sm mb-1">Email</label>
+                    <label className="block text-xs mb-1">Email</label>
                     <input
                         type="email"
                         name="email"
                         value={form.email}
                         onChange={handleChange}
                         required
-                        className="w-full border rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-300"
+                        className="w-full text-xs border rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-300"
                     />
                 </div>
 
                 <div>
-                    <label className="block text-sm mb-1">Password</label>
-                    <input
-                        type="password"
-                        name="password"
-                        value={form.password}
-                        onChange={handleChange}
-                        required
-                        className="w-full border rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-300"
-                    />
+                    <label className="block text-xs mb-1">Password</label>
+                    <div className="relative">
+                        <input
+                            type={showPassword ? "text" : "password"}
+                            name="password"
+                            value={form.password}
+                            onChange={handleChange}
+                            required
+                            className="w-full text-xs border rounded-md px-3 py-2 focus:outline-none focus:ring focus:ring-blue-300"
+                        />
+                        <button
+                            type="button"
+                            onClick={() => setShowPassword((prev) => !prev)}
+                            className='text-gray-500 hover:text-gray-600 focus:outline-none'
+                            style={{
+                                position: "absolute",
+                                right: "10px",
+                                top: "50%",
+                                transform: "translateY(-50%)",
+                                background: "none",
+                                border: "none",
+                                cursor: "pointer",
+                            }}
+                        >
+                            {showPassword ? (
+                                <IoEyeOffOutline size={20} />
+                            ) : (
+                                <IoEyeOutline size={20} />
+                            )}
+                        </button>
+                    </div>
+                </div>
+
+                <div className='flex justify-between items-center align-middle mb-8'>
+                    <div className='flex items-center'>
+                        <input type="checkbox" name="remember" id="remember" />
+                        <label className="text-xs ml-1">Remember me</label>
+                    </div>
+                    <a href="/forgot-password" className="text-xs text-blue-600 hover:underline">
+                        Forgot password?
+                    </a>
                 </div>
 
                 <button
@@ -69,6 +103,17 @@ const LoginPage = () => {
                 >
                     {loading ? 'Loading...' : 'Login'}
                 </button>
+
+                <div className='flex items-center text-center justify-center mt-2'>
+                    <span className='text-xs text-gray-600'>Don't have an account?</span>
+                    <button
+                        type="button"
+                        onClick={() => navigate("/register")}
+                        className="ml-2 text-xs text-blue-600 hover:underline hover:cursor-pointer transition"
+                    >
+                        Register
+                    </button>
+                </div>
             </form>
         </div>
     )
